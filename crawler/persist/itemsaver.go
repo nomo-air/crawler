373ff5e4ec1d0488c11f10ce94aa03d2bea58922
郑万雷 <zhengwl@codoon.com>
2019-07-28 16:57:45 +0800

    去掉es依赖，换做打印

--- crawler/persist/itemsaver.go
+++ crawler/persist/itemsaver.go
@@ -1,19 +1,13 @@
 package persist
 
 import (
-	"context"
-	"github.com/pkg/errors"
 	"go_crawler/crawler/engine"
-	"gopkg.in/olivere/elastic.v5"
 	"log"
 )
 
 func ItemSaver(index string) (chan engine.Item, error) {
 	ch := make(chan engine.Item, 1024)
-	client, err := elastic.NewClient(elastic.SetSniff(false))
-	if err != nil {
-		return nil, err
-	}
+	client := "ElasticClient"
 
 	go func() {
 		itemCount := 0
@@ -30,18 +24,7 @@ func ItemSaver(index string) (chan engine.Item, error) {
 }
 
 // 返回存储的ID
-func save(client *elastic.Client, index string, item engine.Item) error {
-
-	if item.Type == "" {
-		return errors.New("item.type 不能为空")
-	}
-	_, err := client.Index().
-		Index(index).
-		Type(item.Type).
-		Id(item.Id).
-		BodyJson(item).Do(context.Background())
-	if err != nil {
-		return err
-	}
+func save(client, index string, item engine.Item) error {
+	log.Printf("[client:%s] [index:%s] [item:%v] ", client, index, item)
 	return nil
 }
